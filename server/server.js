2e989da02958fc973f6fa31f9aea0627ee051603
Hassam7 <hassam.ali7@hotmail.com>
2017-04-11 00:54:05 +0500

    added get todos route

--- server/server.js
+++ server/server.js
@@ -20,11 +20,18 @@ app.post('/todos', (req, res) => {
         text: req.body.text
 
     }).save().then((success) => {
-        res.send(success);
+        res.send({success});
     }, (error) => {
         res.status(400).send(error);
 
     });
+});
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos)=>{
+      res.send({todos});  
+    },(error)=>{
+        res.status(400).send(error);        
+    }) 
 });
 app.listen(3000, () => {
     console.log("Server started at port 3000");
