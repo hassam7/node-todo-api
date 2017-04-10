0b1b6d566e5f932ebad6229e6ac0b66dcfbf3024
Hassam7 <hassam.ali7@hotmail.com>
2017-04-10 18:23:22 +0500

    MongoDB Count

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -7,14 +7,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
         return console.log("Unable to connect.")
 
     }
+    // db.collection("Todos")
+    //     .find({
+    //         _id: new ObjectID("58eb804b25dde916ccf69851")
+    //     })
+    //     .toArray().
+    // then((result) => console.log(result))
+    //     .then(() => db.close()).
+    // catch((error) => console.log(error))
+    // console.log("Connected to MongoDB Server");
     db.collection("Todos")
-        .find({
-            _id: new ObjectID("58eb804b25dde916ccf69851")
-        })
-        .toArray().
-    then((result) => console.log(result))
+        .find()
+        .count().
+    then((count) => console.log("Todos Count: ",count))
         .then(() => db.close()).
     catch((error) => console.log(error))
     console.log("Connected to MongoDB Server");
-
 });
