23366a7d784f626f75ccb652893c8e04aeec8ed7
Hassam7 <hassam.ali7@hotmail.com>
2017-04-10 18:07:53 +0500

    Object Destructuring and custom ids with ObjectID

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,10 @@
-const MongoClient = require("mongodb").MongoClient;
+// const MongoClient = require("mongodb").MongoClient;
+
+const {MongoClient,ObjectID} = require("mongodb"); //Object destructuring
+
+var obj = new ObjectID();
+console.log(obj);
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
         return console.log("Unable to connect.")
@@ -16,15 +22,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     }
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // })
-    db.collection("Persons").insertOne({
-        name: "Test Person",
-        age: 22,
-        Location: "Classified"
-    }, (err, result) => {
-        if (err)
-            return console.log("Can not insert item");
-        else console.log(result.ops[0]._id.getTimestamp());
-    })
+    // db.collection("Persons").insertOne({
+    //     name: "Test Person",
+    //     age: 22,
+    //     Location: "Classified"
+    // }, (err, result) => {
+    //     if (err)
+    //         return console.log("Can not insert item");
+    //     else console.log(result.ops[0]._id.getTimestamp());
+    // })
 
     db.close();
 });
