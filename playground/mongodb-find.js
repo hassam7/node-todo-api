00e839f90edec61ac1c0189c8b2302dfe310a25c
Hassam7 <hassam.ali7@hotmail.com>
2017-04-10 18:21:48 +0500

    Finding Documents

---
+++ playground/mongodb-find.js
@@ -0,0 +1,20 @@
+const {
+    MongoClient,
+    ObjectID
+} = require("mongodb"); //Object destructuring
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log("Unable to connect.")
+
+    }
+    db.collection("Todos")
+        .find({
+            _id: new ObjectID("58eb804b25dde916ccf69851")
+        })
+        .toArray().
+    then((result) => console.log(result))
+        .then(() => db.close()).
+    catch((error) => console.log(error))
+    console.log("Connected to MongoDB Server");
+
+});
