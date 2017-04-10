97fef9f193e6e8604accc1abde7d8304c3b7e793
Hassam7 <hassam.ali7@hotmail.com>
2017-04-10 18:04:28 +0500

    Creating new collection and inserting item + retreiving time it was created on

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,15 +6,25 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log("Connected to MongoDB Server");
 
-    db.collection("Todos").insertOne({
-        text: 'Somehting todo',
-        completed: false
-    }, (err, result) => {
-        if (err) {
-            return console.log("Unable to insert todo.", err)
+    // db.collection("Todos").insertOne({
+    //     text: 'Somehting todo',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log("Unable to insert todo.", err)
 
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2));
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // })
+    db.collection("Persons").insertOne({
+        name: "Test Person",
+        age: 22,
+        Location: "Classified"
+    }, (err, result) => {
+        if (err)
+            return console.log("Can not insert item");
+        else console.log(result.ops[0]._id.getTimestamp());
     })
+
     db.close();
 });
