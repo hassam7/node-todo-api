f914accb243261f215bd366c2af65405c12bfd9d
Hassam7 <hassam.ali7@hotmail.com>
2017-04-11 00:46:44 +0500

    Addes POST todo route and some refactoring

---
+++ server/server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {
+    mongoose
+} = require('./db/mongoose.js');
+var {
+    Todo
+} = require('./models/todo');
+var {
+    User
+} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+
+    }).save().then((success) => {
+        res.send(success);
+    }, (error) => {
+        res.status(400).send(error);
+
+    });
+});
+app.listen(3000, () => {
+    console.log("Server started at port 3000");
+});
