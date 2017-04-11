862fcca860bdc9d38b73b84fcfb4f860aad11a6d
Hassam7 <hassam.ali7@hotmail.com>
2017-04-11 22:51:14 +0500

    added username and password

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,35 @@
 var mongoose = require('mongoose');
-
-var User = mongoose.model('User',{
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        minlength:1
+var validator = require('validator');
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: function (value) {
+                return validator.isEmail(value);
+            },
+            message: `{VALUE} is not valid Email`
+        }
     },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+
+            type: String,
+            required: true
+        }
+    }]
 
 });
 
